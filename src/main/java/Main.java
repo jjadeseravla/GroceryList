8be8c5384e253028bad018844e0d0e7db76a068e
jjadeseravla <jadealvares@hotmail.co.uk>
2019-04-15 12:43:33 +0100

    make code immutable

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -331,27 +331,34 @@ public class Main {
 
         Scanner scanner = new Scanner(System.in);
 
-        locations.put(0, new Location(0, "on computer learning Java"));
-        locations.put(1, new Location(1, "standing at end of the road"));
-        locations.put(2, new Location(2, "at the tope of the hill"));
-        locations.put(3, new Location(3, "inside a buiilding"));
-        locations.put(4, new Location(4, "in the valley"));
-        locations.put(5, new Location(5, " in the forest"));
+        locations.put(0, new Location(0, "on computer learning Java", tempExit));
 
-        locations.get(1).addExit("W", 2);
-        locations.get(1).addExit("E", 3);
-        locations.get(1).addExit("S", 4);
-        locations.get(1).addExit("N", 5);
+        Map<String, Integer> tempExit = new HashMap<String, Integer>();
 
-        locations.get(2).addExit("N", 5);
+        tempExit = new HashMap<String, Integer>();
+        tempExit.put("W", 2);
+        tempExit.put("E", 3);
+        tempExit.put("S", 4);
+        tempExit.put("N", 5);
+        locations.put(1, new Location(1, "standing at end of the road", tempExit));
 
-        locations.get(3).addExit("W", 1);
+        tempExit = new HashMap<String, Integer>();
+        tempExit.put("N", 5);
+        locations.put(2, new Location(2, "at the tope of the hill", tempExit));
 
-        locations.get(4).addExit("N", 1);
-        locations.get(4).addExit("W", 2);
+        tempExit = new HashMap<String, Integer>();
+        tempExit.put("W", 1);
+        locations.put(3, new Location(3, "inside a buiilding", tempExit));
 
-        locations.get(5).addExit("S", 1);
-        locations.get(5).addExit("W", 2);
+        tempExit = new HashMap<String, Integer>();
+        tempExit.put("N", 1);
+        tempExit.put("W", 2);
+        locations.put(4, new Location(4, "in the valley", tempExit));
+
+        tempExit = new HashMap<String, Integer>();
+        tempExit.put("S", 1);
+        tempExit.put("W", 2);
+        locations.put(5, new Location(5, " in the forest", tempExit));
 
         Map<String, String> vocabulary = new HashMap<String, String>();
         vocabulary.put("QUIT", "Q");
